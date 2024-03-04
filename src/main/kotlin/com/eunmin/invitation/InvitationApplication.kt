b0cba1f47d0e84b152d49e48966eeea548d7aa3d
Heejeong Min <heejeongmin@Heejeongui-MacBookPro.local>
2024-03-04 22:24:16 +0900

    init new page

--- src/main/kotlin/com/eunmin/invitation/InvitationApplication.kt
+++ src/main/kotlin/com/eunmin/invitation/InvitationApplication.kt
@@ -2,10 +2,22 @@ package com.eunmin.invitation
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class InvitationApplication
 
 fun main(args: Array<String>) {
 	runApplication<InvitationApplication>(*args)
+}
+
+@Configuration
+open class ApplicationWebConfig : WebMvcConfigurer {
+	override fun addViewControllers(registry: ViewControllerRegistry) {
+		registry.addViewController("/").setViewName("index")
+		registry.addViewController("/new").setViewName("new")
+		registry.addViewController("/demo").setViewName("demo")
+	}
 }
---
+++ src/main/kotlin/com/eunmin/invitation/web/InvitationController.kt
@@ -0,0 +1,13 @@
+package com.eunmin.invitation.web
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Controller("/v1/invi")
+class InvitationController {
+
+//    @PostMapping("/new")
+//    fun newInvitation() = "newInvitation"
+
+}
